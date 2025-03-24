041a432a821d0862b9bb0c560806b02fa17e770b
COPOKA59 <makarova-polina59@yandex.ru>
2025-03-25 03:29:31 +0500

    v.0.2
    - Создаём представление для "Произведений";
    - Создаём сериализацию для "Произведений";
    - Добавляем отдельный путь для API;
    + изменяем папку Prolog в Sources Root.

---
+++ prolog/works/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from works.models import Works
+
+
+class WorksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Works
+        fields = '__all__'
--- prolog/works/views.py
+++ prolog/works/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
 from django.http import HttpResponse
 from .models import Works
+from .serializers import WorksSerializer
 
-# Create your views here.
+
+# Тестовый просмотр по маршруту.
 def works(request):
     works = Works.objects.all()
     return HttpResponse("<h4>Проверка работы!!!</h4>")
+
+# Представление произведений
+class WorksAPIView(generics.ListAPIView):
+    queryset = Works.objects.all()
+    serializer_class = WorksSerializer
 
